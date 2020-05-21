569f6bb351005f34e653b33c0f7c4613974bddd9
Andris Lapsa <alapsa@scoutrfp.com>
2020-05-22 01:12:21 +0300

    Initial commit

---
+++ Car.js
@@ -0,0 +1,189 @@
+export const BLOCK_SIZE = 20
+export const MOVEMENT_SIZE = 8
+const BOUNDARY_PADDING = MOVEMENT_SIZE - 4
+
+const movementMatrix = {
+  left: { x: -1, y: 0 },
+  right: { x: 1, y: 0 },
+  up: { x: 0, y: -1 },
+  down: { x: 0, y: 1 },
+}
+
+export class Car {
+  trail = []
+  x = 0
+  y = 0
+  color = 'grey'
+  direction = null
+  directionsQueue = []
+  WIDTH = 0
+  HEIGHT = 0
+  blocksToRender = []
+
+  isInTurbo = false
+
+  constructor({ direction, color, x, y, WIDTH, HEIGHT }) {
+    this.color = color
+    this.direction = direction
+    this.x = x
+    this.y = y
+    this.WIDTH = WIDTH
+    this.HEIGHT = HEIGHT
+    this.startingPosition = {
+      x: this.x,
+      y: this.y,
+      direction: this.direction,
+      color: this.color,
+    }
+  }
+
+  changeDirection(direction) {
+    
+    const oldDirection = this.direction
+
+    switch (direction) {
+      case 'left' :
+        if (this.direction === 'right') break
+        this.direction = 'left'
+        break
+      case 'right' :
+        if (this.direction === 'left') break
+        this.direction = 'right'
+        break
+      case 'up' :
+        if (this.direction === 'down') break
+        this.direction = 'up'
+        break
+      case 'down' :
+        if (this.direction === 'up') break
+        this.direction = 'down'
+        break
+    }
+
+    if (this.direction !== oldDirection) {
+      const audio = new Audio(`./turn_${this.color}.m4a`)
+      audio.play()
+    }
+
+    this.directionsQueue.push({ ...movementMatrix[this.direction], direction: this.direction })
+
+  }
+
+  collidesWithSelf() {
+    if (this.x < -BOUNDARY_PADDING) return true
+    if (this.y < -BOUNDARY_PADDING) return true
+
+    if (this.x > this.WIDTH + BOUNDARY_PADDING) return true
+    if (this.y > this.HEIGHT + BOUNDARY_PADDING) return true
+
+    return this.collides(this.x, this.y)
+  }
+
+  // collidesWithTrail(x, y) {
+  //   return trail.find(({ x, y }) => this.collides(x, y))
+  // }
+
+  collides(x, y) {
+    let previousPosition = this.startingPosition
+    return this.trail.find((position, index) => {
+      let collides = false
+      // if (index === this.trail.length - 1) {
+      //   return
+      // }
+
+      if (x === position.x && x === previousPosition.x) {
+        // debugger
+
+        // console.log('x matches', { x,y}, { position, previousPosition })
+        if (previousPosition.direction === 'down') {
+          if (y >= previousPosition.y && y <= position.y) {
+            // console.log('collides with prev', previousPosition.direction)
+            collides = true
+          }
+        }
+
+        if (previousPosition.direction === 'up') {
+          if (y <= previousPosition.y && y >= position.y) {
+            // console.log('collides with prev', previousPosition.direction)
+            collides = true
+          }
+        }
+
+      }
+
+      if (y === position.y && y === previousPosition.y) {
+        // debugger
+
+        // console.log('y matches', { x,y}, { position, previousPosition })
+        if (previousPosition.direction === 'right') {
+          if (x >= previousPosition.x && x <= position.x) {
+            // console.log('collides with prev', previousPosition.direction)
+            collides = true
+          }
+        }
+
+        if (previousPosition.direction === 'left') {
+          if (x <= previousPosition.x && x >= position.x) {
+            // console.log('collides with prev', previousPosition.direction)
+            collides = true
+          }
+        }
+
+      }
+
+
+      previousPosition = position
+      return collides
+      // return position.x === x && position.y === y
+    })
+  }
+
+  turbo() {    
+    if (this.isInTurbo) return
+
+    let n = 15
+    this.isInTurbo = true
+
+    var audio = new Audio('./turbo.m4a');
+    audio.play();
+
+    const a = setInterval(() => {
+      n--
+      if (n === 0) {
+        clearInterval(a)
+        this.isInTurbo = false
+      }
+      const newBlock = this.move()
+      this.blocksToRender.push(newBlock)
+    }, 10)
+  }
+
+  move() {
+    const movement = this.directionsQueue.shift() || movementMatrix[this.direction]
+    const { x, y } = movement
+
+    if (movement.direction) {
+      this.trail.push({
+        x: this.x,
+        y: this.y,
+        direction: movement.direction || this.direction,
+        color: this.color,
+      })
+    }
+
+    this.x += x * MOVEMENT_SIZE
+    this.y += y * MOVEMENT_SIZE
+
+    const newPosition = {
+      x: this.x,
+      y: this.y,
+      direction: movement.direction || this.direction,
+      color: this.color,
+    }
+
+    // this.trail.push(newPosition)
+    this.blocksToRender.push(newPosition)
+
+    return newPosition
+  }
+}
---
+++ index.js
@@ -0,0 +1,216 @@
+import { Car, BLOCK_SIZE } from './Car.js'
+
+const FPS = 60
+const WASD_FOR_FIRST_PLAYER = true
+
+// alert(FPS)
+
+const timestamp = () => performance.now()
+var now,
+    dt   = 0,
+    last = timestamp(),
+    step = 1 / FPS
+
+const blocksToRender = []
+const directionsQueue = []
+
+
+const controlsMapping = {
+  arrowup: { direction: 'up', playerIndex: WASD_FOR_FIRST_PLAYER ? 1 : 0 },
+  arrowleft: { direction: 'left', playerIndex: WASD_FOR_FIRST_PLAYER ? 1 : 0 },
+  arrowdown: { direction: 'down', playerIndex: WASD_FOR_FIRST_PLAYER ? 1 : 0 },
+  arrowright: { direction: 'right', playerIndex: WASD_FOR_FIRST_PLAYER ? 1 : 0 },
+  w: { direction: 'up', playerIndex: WASD_FOR_FIRST_PLAYER ? 0 : 1 },
+  a: { direction: 'left', playerIndex: WASD_FOR_FIRST_PLAYER ? 0 : 1 },
+  s: { direction: 'down', playerIndex: WASD_FOR_FIRST_PLAYER ? 0 : 1 },
+  d: { direction: 'right', playerIndex: WASD_FOR_FIRST_PLAYER ? 0 : 1 },
+}
+
+let gameOver = false
+let paused = false
+
+const scores = [
+  { el: document.querySelector('#score .blue'), score: 0 },
+  { el: document.querySelector('#score .red'), score: 0 },
+]
+function updateScore(playerIndex, score) {
+  const playerScore = scores[playerIndex]
+  const newScore = playerScore.score + score
+  playerScore.el.innerHTML = newScore
+  playerScore.score = newScore
+}
+
+const getNewPlayers = ({ WIDTH, HEIGHT }) => {
+  gameOver = false
+  const STARTING_X_OFFSET = 40
+
+  return [
+    new Car({
+      color: 'blue',
+      direction: 'right',
+      x: STARTING_X_OFFSET,
+      y: Math.floor(HEIGHT / 2),
+      WIDTH,
+      HEIGHT,
+    }),
+    new Car({
+      color: 'red',
+      direction: 'left',
+      x: WIDTH - STARTING_X_OFFSET,
+      y: Math.floor(HEIGHT / 2),
+      WIDTH,
+      HEIGHT,
+    }),
+  ]
+}
+
+function update(ts) {
+  // console.log('update called', ts)
+  if (paused) return
+
+  players.forEach((player, playerIndex) => {
+    if (gameOver) {
+      console.log('game over')
+      return
+    }
+
+    const newBlock = player.move()
+    const otherPlayers = players.filter((_, index) => index !== playerIndex)
+
+    const otherPlayerIndex = playerIndex === 0 ? 1 : 0
+    const collidedWithOtherPlayers = otherPlayers.find((otherPlayer, idx) => {
+      return otherPlayer.collides(player.x, player.y)
+    })
+
+    // console.log('player', { player, otherPlayers })
+
+    const collidedWithSelf = player.collidesWithSelf()
+    if (collidedWithSelf || collidedWithOtherPlayers) {
+      // debugger
+      if (collidedWithSelf) updateScore(otherPlayerIndex, 1)
+      if (collidedWithOtherPlayers) updateScore(playerIndex, 1)
+      // debugger
+      console.log('uh oh!!!')
+      gameOver = true
+      var audio = new Audio('./gameover.m4a');
+      audio.play();
+    } else {
+      blocksToRender.push(newBlock)
+    }
+  })
+
+  // console.log('blocksToRenxder', [...blocksToRender])
+}
+
+function render(ts) {
+  // console.log('render called', ts)
+  let block
+  players.forEach((player) => {
+    const blocksToRender = player.blocksToRender
+
+    while ((block = blocksToRender.pop())) {
+      let { x, y, color } = block
+      // if (color === 'red')
+      // console.log('rendering block', {...block})
+
+      ctx.beginPath()
+      ctx.fillStyle = color === 'blue' ? 'rgba(0, 0, 255, .5)' : 'rgba(255, 0, 0, .3)'
+
+      let blockWidth = BLOCK_SIZE - 2
+      let blockHeight = BLOCK_SIZE - 2
+
+      // if (block.direction === 'right' || block.direction === 'left') {
+      //   blockWidth += 2
+      // }
+
+      // if (block.direction === 'up' || block.direction === 'down') {
+      //   blockHeight += 2
+      // }
+
+      // if (block.direction === 'up') {
+      //   x += 2
+      // }
+
+      // if (block.direction === 'left') {
+      //   x += 2
+      // }
+
+      // if (block.direction === 'down') {
+      //   x += 2
+      //   y -= 2
+      // }
+
+      // if (block.direction === 'right') {
+      //   x -= 2
+      // }
+
+      ctx.arc(x, y, blockWidth - 4, 0, 2 * Math.PI)
+      ctx.fill()
+      ctx.closePath()
+
+      // ctx.fillRect(x, y, blockWidth, blockHeight)
+    }
+  })
+
+}
+
+function frame() {
+  now = timestamp()
+  dt = dt + Math.min(1, (now - last) / 1000)
+  while (dt > step) {
+    dt = dt - step
+    update(step)
+  }
+  render(dt)
+  last = now
+  requestAnimationFrame(frame)
+}
+
+let players = []
+
+document.addEventListener('DOMContentLoaded', () => {
+  const canvas = document.querySelector('canvas')
+  const PADDING = 200
+
+  // canvas.width = 800 // window.innerWidth - PADDING
+  // canvas.height = 800 // window.innerHeight - PADDING
+  const ctx = canvas.getContext('2d')
+  const WIDTH = canvas.width
+  const HEIGHT = canvas.height
+  window.ctx = ctx
+
+  players = getNewPlayers({ WIDTH, HEIGHT })
+  window._players = players
+  requestAnimationFrame(frame)
+
+  window.addEventListener('keydown', ({ key }) => {
+    // console.log('keydown', control)
+
+    const control = controlsMapping[key.toLowerCase()]
+
+    if (control && !gameOver) {
+      const { direction, playerIndex } = control
+      const player = players[playerIndex]
+
+      if (player.direction === direction) {
+        player.turbo()
+      }
+
+      player.changeDirection(direction)
+    }
+
+    switch (key.toLowerCase()) {
+      case 'r' :
+        ctx.clearRect(0, 0, WIDTH, HEIGHT)
+        players = getNewPlayers({ WIDTH, HEIGHT })
+        window._players = players
+        break
+
+      case 'p' :
+        paused = !paused
+        break
+    }
+  })
+})
+
+
